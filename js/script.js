8690ea9f4211d033a7b178d758acec99460868c6
jesswinw <jesswinw@gmail.com>
2020-08-11 12:35:18 +0530

    real data injection

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const table = document.querySelector(".table-container");
+
+const API_URL = "https://sb-analytics-api1.herokuapp.com" + "/api/members";
+
+const makeHtml = (data) => {
+  data.map((item) => {
+    const activity =
+      item.renewal_category !== "Arrears" ? "active" : "inactive";
+
+    table.innerHTML += `<tr>
+    <td>${item.member_id}</td>
+    <td>${item.first_name}</td>
+    <td>${item.middle_name || "-"}</td>
+    <td>${item.last_name}</td>
+    <td>
+        <div class="status-${activity}">${activity.toUpperCase()}</div>
+    </td>
+    </tr>`;
+  });
+};
+
+const members = fetch(API_URL)
+  .then((resp) => resp.json())
+  .then((data) => makeHtml(data));
